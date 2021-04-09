e964628a0aff7e3fa8c82b4da56d4ed06312cd33
zhangzhaoxing <zhangzhaoxing@bytedance.com>
2021-04-10 06:12:53 +0800

    feat: init project

--- geeshorten/app.go
+++ geeshorten/app.go
@@ -1,1 +1,129 @@
-package geeshorten
+package main
+import (
+"database/sql"
+"errors"
+"fmt"
+"log"
+"db"
+"sync"
+"sync/atomic"
+"time"
+)
+
+var MyQueue *queue
+
+type Worker struct {
+	Queue  *queue
+	DB     *sql.DB
+	wg     sync.WaitGroup
+	Closed chan bool
+}
+
+// 构建数据存储实例
+func NewWorker(n int, DB *sql.DB) *Worker {
+	MyQueue = NewMyQueue(n) // 初始缓存队列
+	return &Worker{
+		Queue:  MyQueue,
+		DB:     DB,
+		Closed: make(chan bool, 1),
+	}
+}
+
+// 初始处理数据队列的协程池
+// n: 协程数量
+func (w *Worker) InitWorker(n int) {
+	fmt.Println("数据存储协程池启动...")
+	w.wg.Add(n)
+	for i := 0; i < n; i++ {
+		go w.dbStoreServer()
+	}
+	w.wg.Wait()
+	w.Closed <- true
+	fmt.Println("数据存储协程池已关闭")
+}
+
+// 存储队列中的数据
+func (w *Worker) dbStoreServer() {
+	for {
+		v, err := w.Queue.Pull()
+		if err != nil { // 缓存队列已经关闭
+			break
+		} else if v == nil { // 队列为空
+			time.Sleep(200 * time.Millisecond)
+			continue
+		}
+
+		err = v.Insert(w.DB)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	w.wg.Done()
+}
+
+/**
+待处理数据队列结构体
+*/
+type queue struct {
+	size   int64            // 队列大小
+	queue  chan *db.Request // 缓存channel
+	closed uint32           // 队列是否关闭 0.正常 1.关闭
+	lock   sync.RWMutex     // 读写锁
+}
+
+// 获取实例
+func NewMyQueue(n int) *queue {
+	return &queue{
+		queue: make(chan *db.Request, n),
+	}
+}
+
+// 将数据写入队列
+func (q *queue) Push(v *db.Request) (ok bool, err error) {
+	q.lock.RLock()
+	defer q.lock.RUnlock()
+
+	if q.closed == 1 {
+		return false, errors.New("消息队已关闭")
+	}
+	select {
+	case q.queue <- v:
+		q.size++
+		ok = true
+	default:
+		ok = false
+	}
+	return
+}
+
+// 拉取队列信息
+func (q *queue) Pull() (*db.Request, error) {
+	select {
+	case v, ok := <-q.queue:
+		if !ok {
+			return nil, errors.New("消息队已关闭") // 队列关闭时
+		}
+		q.size--
+
+		return v, nil
+	default:
+		return nil, nil // 队列为空时
+	}
+}
+
+// 获取队列中消息数量
+func (q *queue) Size() int64 {
+	return q.size
+}
+
+// 关闭队列
+func (q *queue) Close() bool {
+	if atomic.CompareAndSwapUint32(&q.closed, 0, 1) {
+		q.lock.Lock()
+		close(q.queue)
+		q.lock.Unlock()
+		return true
+	}
+	return false
+}
+
---
+++ geeshorten/go.mod
@@ -0,0 +1,3 @@
+module github.com/ZhaoxingZhang/go-gees/geeshorten
+
+go 1.15
